9f008e7e99231562539a9ac246fd300b811a6122
rallen0150 <rallen0150@gmail.com>
2016-09-24 12:54:14 -0400

    gameplay goes through once, but no change in ace

--- blackjack.py
+++ blackjack.py
@@ -5,10 +5,12 @@ Q = 10
 K = 10
 A = 11
 
+
+
 suits = ('Club', 'Spade', 'Heart', 'Diamond')
 ranks = (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K)
-# values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-#          '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+# values = {'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+#          '10': 10, 'J': 10, 'Q': 10, 'K': 10}
 # ranks = values.items()
 
 class Player:
@@ -22,15 +24,15 @@ class Player:
         return bet
 
 
-class Cards:
-
-    def __init__(self, suit, rank):
-        pass
+# class Card:
+#     def __init__(self, suit, rank):
+#         pass
 
 class Deck:
 
     def __init__(self):
         self.cards = [(rank, suit) for suit in suits for rank in ranks]
+
 
     def deck_shuffle(self):
         shuffle(self.cards)
@@ -44,16 +46,17 @@ class Hand:
     def __init__(self):
         self.hand = []
 
+
     def dealt_card(self, card):
         self.hand.append(card)
         return self.hand
 
-    # def add_values(self):
-    #     value = 0
-    #     for card in self.hand:
-    #         value += card[0]
-    #     return value
 
+    def add_values(self):
+        value = 0
+        for card in self.hand:
+            value += card[0]
+        return value
 
 
 
@@ -68,6 +71,7 @@ deck = Deck()
 hand = Hand()
 dealer_hand = Hand()
 hand_value = 0
+dealer_hand_value = 0
 
 deck.deck_shuffle()
 
@@ -83,25 +87,47 @@ for i in range(1):
     hand.dealt_card(deck.deal_card())
     dealer_hand.dealt_card(deck.deal_card())
 
-# hand_value += hand.add_values()
-#
-# print("the value of hand is: {}".format(hand_value))
-
 
 print("{}'s Hand".format(user.name))
 print(*hand.dealt_card(deck.deal_card()))
 
+hand_value += hand.add_values()
+print("the value of hand is: {}".format(hand_value))
+
 print("Dealer's Hand")
 print(*dealer_hand.dealt_card(deck.deal_card()))
+dealer_hand_value += dealer_hand.add_values()
+print("the value of DEALER's hand is: {}".format(dealer_hand_value))
 
-while True:
-# while user.hand.add_values() <= 21:
-    move = input("Would you like to (h)it or (s)tand? ").lower()
-    if move == 'h':
-        print("{}'s Hand".format(user.name))
-        print(*hand.dealt_card(deck.deal_card()))
-        # Add the values here
-        continue
-    else:
-        print("You chose to stand. Now it's my turn!")
+# Now the GamePlay for 1 turn
+for x in range(1):
+    while hand.add_values() <= 21:
+        move = input("Would you like to (h)it or (s)tand? ").lower()
+        if move == 'h':
+            print("{}'s Hand".format(user.name))
+            print(*hand.dealt_card(deck.deal_card()))
+            hand_value = hand.add_values()
+            print("the value of hand is: {}".format(hand_value))
+            # Add the values here
+            continue
+        elif move == "s":
+            print("You chose to stand. Your hand's value is {}\nNow it's my turn!".format(hand_value))
+            break
+
+    if hand_value > 21:
+        print("You Busted, You Lose!")
         break
+
+    while dealer_hand.add_values() < 17 or dealer_hand_value < hand_value:
+        print("Dealer's Hand")
+        print(*dealer_hand.dealt_card(deck.deal_card()))
+        dealer_hand_value = dealer_hand.add_values()
+
+    print("The value of the dealer's hand is {}".format(dealer_hand_value))
+
+    if dealer_hand_value > 21:
+        print("The Dealer Busted! YOU WIN!")
+    elif dealer_hand_value >= hand_value:
+        print("The Dealer Wins. YOU LOSE!")
+    elif hand_value > dealer_hand_value:
+        print("YOU WIN!")
