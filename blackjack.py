a4841a4894a226d25eed3326d78db7498210d1f6
rallen0150 <rallen0150@gmail.com>
2016-09-24 09:56:40 -0400

    got the dealer to deal cards into the players' hands.

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,15 @@
-import random
+from random import shuffle
 
-suits = (['Club'], ['Spade'], ['Heart'], ['Diamond'])
-ranks = (['A'], ['2'], ['3'], ['4'], ['5'], ['6'], ['7'], ['8'], ['9'], ['10'], ['J'], ['Q'], ['K'])
+J = 10
+Q = 10
+K = 10
+A = 11
+
+suits = ('Club', 'Spade', 'Heart', 'Diamond')
+ranks = (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K)
+# values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+#          '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+# ranks = values.items()
 
 class Player:
 
@@ -9,17 +17,15 @@ class Player:
         self.name = name
         self.bet = bet
 
+    def  new_bet(self):
+        bet = self.bet - 10
+        return bet
 
-# class Cards:
-#
-#     def __init__(self):
-#         self.suit = ['Club', 'Spade', 'Heart', 'Diamond']
-#         self.rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-#
-#     def get_card(self):
-#         card_hand = random.choice(cards.rank), random.choice(cards.suit)
-#         return card_hand
 
+class Cards:
+
+    def __init__(self, suit, rank):
+        pass
 
 class Deck:
 
@@ -27,7 +33,7 @@ class Deck:
         self.cards = [(rank, suit) for suit in suits for rank in ranks]
 
     def deck_shuffle(self):
-        random.shuffle(self.cards)
+        shuffle(self.cards)
 
     def deal_card(self):
         return self.cards.pop(0)
@@ -42,28 +48,60 @@ class Hand:
         self.hand.append(card)
         return self.hand
 
+    # def add_values(self):
+    #     value = 0
+    #     for card in self.hand:
+    #         value += card[0]
+    #     return value
 
-user = Player("Robbie", 100)
+
+
+
+user_name = input("Player, what's your name?  ")
+user = Player(user_name, 100)
 dealer = Player("Dealer", 0)
 
 # cards = Cards()
+
 deck = Deck()
+
 hand = Hand()
 dealer_hand = Hand()
-
-print(user.name)
-print(dealer.name)
-
-# card_hand = cards.get_card(), cards.get_card()
+hand_value = 0
 
 deck.deck_shuffle()
 
-print(deck.cards)
+# Prints the shuffled deck of cards
+# print(deck.cards)
+
+# print("You have ${} to bet".format(user.bet))
+# Bet loses $10
+user.new_bet()
+print("Now you have ${} to bet".format(user.new_bet()))
+
+for i in range(1):
+    hand.dealt_card(deck.deal_card())
+    dealer_hand.dealt_card(deck.deal_card())
+
+# hand_value += hand.add_values()
+#
+# print("the value of hand is: {}".format(hand_value))
 
 
-hand.dealt_card(deck.deal_card())
-dealer_hand.dealt_card(deck.deal_card())
-print("Robbie's Hand")
-print(hand.dealt_card(deck.deal_card()))
+print("{}'s Hand".format(user.name))
+print(*hand.dealt_card(deck.deal_card()))
+
 print("Dealer's Hand")
-print(dealer_hand.dealt_card(deck.deal_card()))
+print(*dealer_hand.dealt_card(deck.deal_card()))
+
+while True:
+# while user.hand.add_values() <= 21:
+    move = input("Would you like to (h)it or (s)tand? ").lower()
+    if move == 'h':
+        print("{}'s Hand".format(user.name))
+        print(*hand.dealt_card(deck.deal_card()))
+        # Add the values here
+        continue
+    else:
+        print("You chose to stand. Now it's my turn!")
+        break
